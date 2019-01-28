49eb58a0353639f53f882d25df002deec47ac650
06hannibal <06hannibal@gmail.com>
2019-01-28 09:26:06 +0200

    change the method forEach for each

--- web/modules/custom/wiki_content/js/wiki_content.js
+++ web/modules/custom/wiki_content/js/wiki_content.js
@@ -7,22 +7,22 @@
         attach:function() {
             $(function () {
                 $("div.mw-parser-output a").once().hover(function(){
-                    wiki_link = $(this);
+                    wiki_link = $( this );
                     url_title_wiki = wiki_link.attr('href');
                     title_wiki = url_title_wiki.substr(6);
-                        $(this).append( $("<div class='popap-wiki'>"+
-                            jQuery.ajax({
-                        url: "https://en.wikipedia.org/w/api.php?format=json&action=parse&page="+ title_wiki,
-                        dataType: "jsonp",
-                        success: function (rows) {
-                            page_wiki = rows;
-                            Object.keys(page_wiki).forEach(function (key){
-                                $('div.popap-wiki').append(page_wiki[key].text['*']);
-                            });
-                        }
-                    })+"</div>"));
-                },function() {
-                    $(this).find("div.popap-wiki").remove();
+                    $( this ).append( $("<div class='popap-wiki'>"+
+                        jQuery.ajax({
+                            url: "https://en.wikipedia.org/w/api.php?format=json&action=parse&page="+ title_wiki,
+                            dataType: "jsonp",
+                            success: function (rows) {
+                                page_wiki = rows;
+                                $(page_wiki).each(function() {
+                                    $('div.popap-wiki').append(page_wiki.parse.text['*']);
+                                });
+                            }
+                        })+"</div>"));
+                    },function() {
+                    $( this ).find("div.popap-wiki").remove();
                 })
             });
         }
